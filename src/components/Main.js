7e5a82af5ba2e2030e85d9540044e18caf735947
bookpark <bkbkgg@gmail.com>
2022-11-18 17:18:48 +0900

    Section6 추가 및 componentBox, hover css 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,8 @@ import Section1 from './Section1';
 import Section2 from './Section2';
 import Section3 from './Section3';
 import Section4 from './Section4';
+import Section5 from './Section5';
+import Section6 from './Section6';
 
 class Main extends Component {
     render() {
@@ -17,6 +19,8 @@ class Main extends Component {
                         <Section2 />
                         <Section3 />
                         <Section4 />
+                        <Section5 />
+                        <Section6 />
                     </tbody>
                 </Table>
             </div>
