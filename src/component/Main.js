5db30f2089943997467db7482c9bb195401e272a
bookpark <bkbkgg@gmail.com>
2022-11-18 13:28:31 +0900

    Header, Main component 추가

---
+++ src/component/Main.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className='main'>
+                Main
+            </div>
+        )
+    }
+}
+
+export default Main;
